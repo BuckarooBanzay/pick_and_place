4938c09b270c919c4d924f24558e958d434ef48d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-21 17:27:51 +0200

    debug

--- tool.lua
+++ tool.lua
@@ -5,11 +5,15 @@ minetest.register_tool("pick_and_place:placer", {
     stack_max = 1,
     range = 0,
     on_use = function(itemstack, player)
+        print("on_use: " .. itemstack:get_name() .. ", " .. player:get_player_name())
     end,
     on_focus = function(itemstack, player)
+        print("on_focus: " .. itemstack:get_name() .. ", " .. player:get_player_name())
     end,
     on_step = function(itemstack, player)
+        print("on_step: " .. itemstack:get_name() .. ", " .. player:get_player_name())
     end,
-    on_blur = function(player)
+    on_blur = function(itemstack, player)
+        print("on_blur: " .. itemstack:get_name() .. ", " .. player:get_player_name())
     end
 })
